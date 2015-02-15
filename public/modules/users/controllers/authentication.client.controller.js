51ce25ba0b1d71dad35b96f9afa126e4d1e5d90b
Nelis <nelisengels210@gmail.com>
2015-02-15 17:38:58 +0100

    update signup function

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -3,12 +3,16 @@
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
 	function($scope, $http, $location, Authentication) {
 		$scope.authentication = Authentication;
-
+		$scope.channel_levels = new Array(1,2,3,4,5);
+		$scope.channel_infos = new Array();
+		$scope.signup_step = 1;
+		$scope.isopen = false;
 		// If user is signed in then redirect back home
 		if ($scope.authentication.user) $location.path('/');
 
 		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
+			$scope.signup_step = 2;
+			/*$http.post('/auth/signup', $scope.credentials).success(function(response) {
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
 
@@ -16,7 +20,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				$location.path('/');
 			}).error(function(response) {
 				$scope.error = response.message;
-			});
+			});*/
 		};
 
 		$scope.signin = function() {
